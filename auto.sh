307eaa400c03b7d9a0447a61d70e23d1c83a151f
iiborn <abbasgit91@gmail.com>
2017-03-11 16:50:28 +0000

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@
 COUNTER=1
 while(true) do
 ./launch.sh
-curl "https://api.telegram.org/bot232820139:AAFnJNel5eS2dmCL-8uT4dRFfs2ABUhwgE4/sendmessage" -F "chat_id=-1001093510408" -F "text=Self Crashed
+curl "https://api.telegram.org/bot232820139:AAFnJNel5eS2dmCL-8uT4dRFfs2ABUhwgE4/sendmessage" -F "chat_id=-1001093510408" -F "text=Selfbot!! Crashed
 Tying For Running...
 Bot online Again!
 Check Number : ${COUNTER}"
